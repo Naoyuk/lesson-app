164afe95cad155831d2f6076046476519687db58
Naoyuki Ishida <39.ishida@gmail.com>
2021-01-31 23:24:46 -0800

    show all lessons with map method

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,40 @@ import Lesson from './Lesson';
 
 class Main extends React.Component {
   render() {
-    const lessonItem = {
+    const lessonItems = [
+      {
       name: 'HTML & CSS',
       image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg',
-    };
+      introduction: 'HTMLとCSSについての勉強をします',
+      },
+      {
+      name: 'JavaScript',
+      image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg',
+      introduction: 'JavaScript(ES6)についての勉強をします',
+      },
+      {
+      name: 'React',
+      image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg',
+      introduction: 'Reactの勉強をします',
+      }
+    ];
     return(
-      <Lesson
-        name = {lessonItem.name}
-        image = {lessonItem.image}
-      />
+      <div className='main-wrapper'>
+        <div className='main'>
+          <div className='lesson-container'>
+            <h3>学べるレッスン</h3>
+            {lessonItems.map((lessonItem) => {
+              return(
+                <Lesson
+                  name = {lessonItem.name}
+                  image = {lessonItem.image}
+                  introduction = {lessonItem.introduction}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     )
   }
 }
