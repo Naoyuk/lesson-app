86331145d75ea0dc0d46f1aec180dbadeaf984b2
Naoyuki Ishida <39.ishida@gmail.com>
2021-02-01 07:55:58 -0800

    implement forms and sent message

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
 class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isSubmitted: false }
+  }
+
+  submitInquiry() {
+    this.setState({ isSubmitted: true });
+  }
+
   render(){
+    let contactForm;
+    if(this.state.isSubmitted){
+      contactForm = (
+        <p className='contact-submit-message'>The inquiry is sent.</p>
+      );
+    } else {
+      contactForm = (
+        <form onSubmit={() => {this.submitInquiry()}}>
+          <label>
+            E-mail address:
+            <input type='text' name='email' />
+          </label>
+          <label>
+            Inquiry:
+            <textarea name='inquiry' />
+          </label>
+          <input type='submit' value='Send!' />
+        </form>
+      );
+    }
+
     return(
       <div className='contact-container'>
-        <p>__here</p>
-        <form className='contact-form'>
-        </form>
+        <div className='contact-form'>
+          {contactForm}
+        </div>
       </div>
     );
   }
