48fcdda426925202e192a9dc7886ee52a4ee7330
Naoyuki Ishida <39.ishida@gmail.com>
2021-01-31 21:05:14 -0800

    add Footer, Header, Lesson and Main

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import Language from './Language';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 class App extends React.Component {
   render() {
     return(
       <div>
-        <h1>言語一覧</h1>
-        <Language 
-          name = 'HTML & CSS'
-          image = 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
-        />
-        <Language 
-          name = 'ES6'
-          image = 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
-        />
-        <Language 
-          name = 'React'
-          image = 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
-        />
+        <Header />
+        <Main />
+        <Footer />
       </div>
     )
   }
